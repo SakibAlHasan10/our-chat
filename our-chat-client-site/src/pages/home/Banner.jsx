772c8b349abe221a9ead50601b7bb680b2696707
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-02-08 22:10:19 +0600

    added style

--- our-chat-client-site/src/pages/home/Banner.jsx
+++ our-chat-client-site/src/pages/home/Banner.jsx
@@ -3,7 +3,7 @@ const Banner = () => {
     <div className="bg-home-banner min-h-screen w-[100vw] bg-cover bg-center bg-no-repeat">
       <div className="flex h-screen justify-center items-center">
         <div className="flex-1 text-white ">
-          <h2 className="lg:text-[80px]">Message privately</h2>
+          <h2 className="lg:text-[80px] tracking-tighter leading-none">Message <br /> privately</h2>
           <p>
             Simple, reliable, private messaging and calling for free*, available
             all over the world.
