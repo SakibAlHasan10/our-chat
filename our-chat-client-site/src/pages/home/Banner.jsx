cfc3a5a994870d7ced592faad1b1bc319a8d9b5b
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-02-09 21:44:11 +0600

    added private route

--- our-chat-client-site/src/pages/home/Banner.jsx
+++ our-chat-client-site/src/pages/home/Banner.jsx
@@ -1,10 +1,10 @@
 const Banner = () => {
   return (
-    <div className="bg-home-banner min-h-screen w-[100vw] bg-cover bg-center bg-no-repeat">
-      <div className="flex h-screen justify-center items-center">
+    <div className="bg-home-banner min-h-screen w-full bg-cover bg-center bg-no-repeat mb-96">
+      <div className="flex h-screen justify-center items-center lg:px-10">
         <div className="flex-1 text-white ">
-          <h2 className="lg:text-[80px] tracking-tighter leading-none">Message <br /> privately</h2>
-          <p>
+          <h2 className="lg:text-[80px] tracking-tighter leading-none lg:w-[322px]">Message privately</h2>
+          <p className="lg:w-[322px] mt-7 font-medium text-lg">
             Simple, reliable, private messaging and calling for free*, available
             all over the world.
           </p>
